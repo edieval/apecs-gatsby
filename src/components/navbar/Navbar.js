4ea67219f77cfa831a55871459219d33f6f46a34
Thib <146090290+Thiiiib@users.noreply.github.com>
2023-10-09 15:17:01 +0200

    Update Navbar.js
    fix(design): logo file path modification
    
    Refs: #8

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../../images/APECS-Logo-horizontal.jpg";
+import logo from "../../images/APECS-logo-header.png";
 import "./navbar.scss";
 import slugify from "../../utils/Slugify";
 import { graphql, Link, useStaticQuery } from "gatsby";
