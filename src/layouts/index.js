a80986c6ed2c0e72bc4e5f36dab945aa060a4198
Thib <146090290+Thiiiib@users.noreply.github.com>
2023-10-13 15:48:31 +0200

    test: ShopIcon use

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -20,9 +20,7 @@ const Layout = ({ children }) => {
           <div className="level-right">
             <p className="level-item">
               <Link className="top-bandeau-link" to="/support/shop/Shop">
-                <span className="icon is-medium">
-                  <ShopIcon />
-                </span>
+                  <FontAwesomeIcon icon=ShopIcon color="white" size="2x" />
                 //<FontAwesomeIcon icon={faGift} color="white" size="2x" />
                 &nbsp;
                 <span className="is-hidden-touch">Boutique</span>
